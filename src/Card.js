cd2aa45a4b47f6c544cb50eb8c5c33ccbeb4e49e
Galen Guyer <galen@galenguyer.com>
2020-11-14 13:22:54 -0500

    draw more attention to numbers instead of label

--- src/Card.js
+++ src/Card.js
@@ -8,15 +8,17 @@ const Card = (props) => {
     }
     return (
         <div className="Card bg-white rounded-lg border-2 border-orange-300 p-4 m-4">
-            <h3 className="text-lg">{props.name}</h3>
             <p>
-                {props.latest}
-                {props.suffix}{" "}
+                <span className="text-2xl">
+                    {props.latest}
+                    {props.suffix}{" "}
+                </span>
                 <span className="Diff text-gray-600 text-sm">
                     ({diff}
                     {props.suffix})
                 </span>
             </p>
+            <h3 className="text-base">{props.name}</h3>
         </div>
     );
 };
